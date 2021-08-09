3b08a5c8561cdff6a13d6caaa4d8cef0d630519c
anarchytrex <anarchytrex@hotmail.com>
2021-08-09 12:15:01 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+    // The data/time we want to countdown to
+    var countDownDate = new Date("Feb 30, 2022 00:00:00").getTime();
+
+    // Run myfunc every second
+    var myfunc = setInterval(function() {
+
+    var now = new Date().getTime();
+    var timeleft = countDownDate - now;
+        
+    // Calculating the days, hours, minutes and seconds left
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+        
+    // Result is output to the specific element
+    document.getElementById("dayss").innerHTML = days
+    document.getElementById("hourss").innerHTML = hours
+    document.getElementById("minutess").innerHTML = minutes
+    document.getElementById("secondss").innerHTML = seconds
+        
+    // Display the message when countdown is over
+    if (timeleft < 0) {
+        clearInterval(myfunc);
+        document.getElementById("days").innerHTML = ""
+        document.getElementById("hours").innerHTML = "" 
+        document.getElementById("mins").innerHTML = ""
+        document.getElementById("secs").innerHTML = ""
+        document.getElementById("end").innerHTML = "TIME UP!!";
+    }
+    }, 1000);
