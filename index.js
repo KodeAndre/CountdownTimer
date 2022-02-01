22a339f392bb362599851c3ee7c98bb58352781e
kodeandre <andre.kodehode@gmail.com>
2022-02-01 10:42:43 +0100

    Chore: updated title and text to reflect real purpose

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
     // The data/time we want to countdown to
-    var countDownDate = new Date("Feb 22, 2022 00:00:00").getTime();
+    let countDownDate = new Date("Feb 22, 2022 00:00:00").getTime();
 
     // Run myfunc every second
-    var myfunc = setInterval(function() {
+    let myfunc = setInterval(function() {
 
-    var now = new Date().getTime();
-    var timeleft = countDownDate - now;
-        
+    let now = new Date().getTime();
+    let timeleft = countDownDate - now;
+
     // Calculating the days, hours, minutes and seconds left
-    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-        
+    let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
     // Result is output to the specific element
-    document.getElementById("dayss").innerHTML = days
-    document.getElementById("hourss").innerHTML = hours
-    document.getElementById("minutess").innerHTML = minutes
-    document.getElementById("secondss").innerHTML = seconds
+    document.getElementById("dayss").innerHTML = days.toString();
+    document.getElementById("hourss").innerHTML = hours.toString();
+    document.getElementById("minutess").innerHTML = minutes.toString();
+    document.getElementById("secondss").innerHTML = seconds.toString();
 
     // Display the message when countdown is over
     if (timeleft < 0) {
         clearInterval(myfunc);
         document.getElementById("dayss").innerHTML = ""
-        document.getElementById("hourss").innerHTML = "" 
+        document.getElementById("hourss").innerHTML = ""
         document.getElementById("minutess").innerHTML = ""
         document.getElementById("secondss").innerHTML = ""
         document.getElementById("title").innerHTML = "Countdown Ended."
